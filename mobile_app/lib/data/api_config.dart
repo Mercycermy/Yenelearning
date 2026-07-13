0624dcb61f386fa74d050c74008872f67c2d8b5b
mercycermy <mercycermy5@gmail.com>
2026-07-13 03:06:28 -0700

    simplify baseUrl configuration for API access

--- mobile_app/lib/data/api_config.dart
+++ mobile_app/lib/data/api_config.dart
@@ -1,8 +1,3 @@
-import 'package:flutter/foundation.dart';
-
 class ApiConfig {
-  static const String _webBaseUrl = 'http://localhost:3001/api';
-  static const String _deviceBaseUrl = 'http://10.0.2.2:3001/api';
-
-  static String get baseUrl => kIsWeb ? _webBaseUrl : _deviceBaseUrl;
+  static const String baseUrl = 'https://yene-backend.onrender.com/api';
 }
